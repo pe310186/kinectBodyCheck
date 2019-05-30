cee7f49dcad5016e631e7b157a6eca72c291d431
pe310186 <pe310186@gmail.com>
2019-05-30 14:33:56 +0800

    init

---
+++ kinect/Assets/KinectView/Scripts/ColorSourceView.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+
+public class ColorSourceView : MonoBehaviour
+{
+    public GameObject ColorSourceManager;
+    private ColorSourceManager _ColorManager;
+    
+    void Start ()
+    {
+        Canvas canvas = FindObjectOfType<Canvas>();
+        float h = canvas.GetComponent<RectTransform>().rect.height;
+        float w = canvas.GetComponent<RectTransform>().rect.width;
+        transform.localScale = new Vector3(w,h,1);
+        gameObject.GetComponent<Renderer>().material.SetTextureScale("_MainTex", new Vector2(-1, 1));
+    }
+    
+    void Update()
+    {
+
+        if (ColorSourceManager == null)
+        {
+            return;
+        }
+        
+        _ColorManager = ColorSourceManager.GetComponent<ColorSourceManager>();
+        if (_ColorManager == null)
+        {
+            return;
+        }
+        
+        gameObject.GetComponent<Renderer>().material.mainTexture = _ColorManager.GetColorTexture();
+    }
+}
---
+++ kinect/Assets/testBody.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Kinect = Windows.Kinect;
+
+public class testBody : MonoBehaviour {
+
+    BodySourceView bodySource;
+    // Use this for initialization
+    void Start () {
+        bodySource = GetComponent<BodySourceView>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("LeftHand " + bodySource.GetJointPosition(Kinect.JointType.HandLeft));
+        Debug.Log("Head " + bodySource.GetJointPosition(Kinect.JointType.Head));
+    }
+}
